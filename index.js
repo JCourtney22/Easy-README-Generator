7591fa1be48bd06d5eebb7b41eee58c9b44771c9
JCourtney22 <115502589+JCourtney22@users.noreply.github.com>
2023-01-29 14:16:47 +0000

    Restructured folders, installed node.

--- index.js
+++ index.js
@@ -5,7 +5,33 @@ const generateMarkdown = require("./utils/generateMarkdown");
 
 // array of questions for user
 const questions = [
-
+    {
+        type: 'input',
+        message: 'What is the of your project ?',
+        name: 'title',
+    },
+    {
+        type: 'input',
+        message: 'Please describe your project.',
+        name: 'description',
+    },
+    {
+        type: 'input',
+        message: 'Please describe the installation process of your project.',
+        name: 'installation',
+    },
+    {
+        type: 'input',
+        message: 'How should your project be used?',
+        name: 'usage',
+    },
+    {
+        type: 'input',
+        message: 'Please choose a license.',
+        name: 'license',
+        options: []
+    },
+// I need to include contributing, tests and questions but I also need to look into this.
 ];
 
 // function to write README file
